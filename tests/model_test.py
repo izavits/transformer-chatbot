5a10ef2098b06bdb80a05806172c2481930f34cc
izavits <izavits@gmail.com>
2019-10-06 16:43:32 +0300

    Add unit test for model name

--- tests/model_test.py
+++ tests/model_test.py
@@ -13,9 +13,14 @@ class TestModel(unittest.TestCase):
     def test_positional_encoding(self):
         """Check the positional encoding"""
         pe = PositionalEncoding(2, 2)
-
         self.assertEqual(str(pe.pos_encoding.numpy()[0]), '''[[0.         1.        ]
  [0.84147096 0.5403023 ]]''')
+
+    def test_model_name_parsing(self):
+        """Check the parsing of the model name"""
+        datafile = './data/SAMPLE_SET.txt'
+        model_name = datafile.split('/')[-1].split('.')[0]
+        self.assertEqual(model_name, 'SAMPLE_SET')
 
 
 suite = unittest.TestLoader().loadTestsFromTestCase(TestModel)
